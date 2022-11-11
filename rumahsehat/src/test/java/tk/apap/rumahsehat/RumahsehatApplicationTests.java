02e28d2ce0068b748bd9da29e9f1eabc000c6ab2
oba20201 <obapakpahan01@gmail.com>
2022-11-11 07:52:23 +0700

    feat: tutorial 1 - initialize spring boot project

---
+++ rumahsehat/src/test/java/tk/apap/rumahsehat/RumahsehatApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.apap.rumahsehat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumahsehatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
