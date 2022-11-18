2077261f83b944321ea64a8723ddff7550377f47
Dyta Dewipuspita <dyta.dewipuspita01@ui.ac.id>
2022-11-18 17:04:14 +0700

    want to pull

---
+++ rumahsehat/src/main/java/tk/apap/rumahsehat/repository/AppointmentDb.java
@@ -0,0 +1,17 @@
+package tk.apap.rumahsehat.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import tk.apap.rumahsehat.model.AppointmentModel;
+import tk.apap.rumahsehat.model.UserModel;
+
+@Repository
+public interface AppointmentDb extends JpaRepository<AppointmentModel, Long> {
+    AppointmentModel findByPatientName(String patientName);
+
+    List<AppointmentModel> findAll();
+    //void delete(UserModel user);
+}
