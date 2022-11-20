c68113d5554d7ba811adabe46140e842e6371c06
oba20201 <obapakpahan01@gmail.com>
2022-11-20 21:52:01 +0700

    feat: add rest api implementation for Pasien

--- rumahsehat/src/main/java/tk/apap/rumahsehat/service/PasienRestService.java
+++ rumahsehat/src/main/java/tk/apap/rumahsehat/service/PasienRestService.java
@@ -6,7 +6,6 @@ import tk.apap.rumahsehat.model.PasienModel;
 import javax.transaction.Transactional;
 import java.util.List;
 
-@Service@Transactional
 public interface PasienRestService {
     List<PasienModel> retrieveListPasien();
 }
--- rumahsehat/src/main/java/tk/apap/rumahsehat/service/PasienRestServiceImpl.java
+++ rumahsehat/src/main/java/tk/apap/rumahsehat/service/PasienRestServiceImpl.java
@@ -1,11 +1,15 @@
 package tk.apap.rumahsehat.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import tk.apap.rumahsehat.model.PasienModel;
 import tk.apap.rumahsehat.repository.PasienDb;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Service
+@Transactional
 public class PasienRestServiceImpl implements PasienRestService{
     @Autowired
     PasienDb pasienDb;
