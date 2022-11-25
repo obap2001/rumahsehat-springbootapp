2ba51115a548a55b3169757720799ce5af3e2d1c
oba20201 <obapakpahan01@gmail.com>
2022-11-25 21:47:19 +0700

    feat: implement web service top up saldo

--- rumahsehat/src/main/java/tk/apap/rumahsehat/restcontroller/PasienRestController.java
+++ rumahsehat/src/main/java/tk/apap/rumahsehat/restcontroller/PasienRestController.java
@@ -4,14 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import tk.apap.rumahsehat.model.PasienModel;
 import tk.apap.rumahsehat.service.PasienRestService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @RestController
@@ -34,5 +33,17 @@ public class PasienRestController {
             responseEntity = ResponseEntity.badRequest().body(HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return responseEntity;
+    }
+
+    //top up saldo
+    @PutMapping(value = "/{idPasien}/update-saldo")
+    private PasienModel topUpSaldoPasien(@PathVariable("idPasien") Long idPasien, @RequestParam Integer newSaldo){
+        try{
+            return pasienRestService.updateSaldo(idPasien, newSaldo);
+        }catch (NoSuchElementException e){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Pasien dengan id " + idPasien + " tidak ditemukan."
+            );
+        }
     }
 }
