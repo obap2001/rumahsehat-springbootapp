e3c036bb0ec9df23bf05296f2faffefd374246e3
Oba Pakpahan <oba.pakpahan@ui.ac.id>
2022-11-25 08:08:04 +0700

    Feat/milestone2 oba fitur13

--- rumahsehat/src/main/java/tk/apap/rumahsehat/restcontroller/ObatRestController.java
+++ rumahsehat/src/main/java/tk/apap/rumahsehat/restcontroller/ObatRestController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import tk.apap.rumahsehat.service.ObatRestService;
 import java.util.List;
 
 @Slf4j
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class ObatRestController {
@@ -35,5 +37,9 @@ public class ObatRestController {
             responseEntity = ResponseEntity.badRequest().body(HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return responseEntity;
+    }
+    @GetMapping("/dataobat")
+    private List<ObatModel> retrieveListObat(){
+        return obatRestService.retrieveListObat();
     }
 }
