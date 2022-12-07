0de33cd1e61f683f1febd49d36d28ba48e411e8f
Amelia Maharani Nurmalitasari <amelia.maharani02@ui.ac.id>
2022-12-07 16:13:26 +0700

    feat: configure register pasien

--- rumahsehat/src/main/java/tk/apap/rumahsehat/service/PasienRestService.java
+++ rumahsehat/src/main/java/tk/apap/rumahsehat/service/PasienRestService.java
@@ -1,12 +1,13 @@
 package tk.apap.rumahsehat.service;
 
-import org.springframework.stereotype.Service;
+// import org.springframework.stereotype.Service;
 import tk.apap.rumahsehat.model.PasienModel;
 
-import javax.transaction.Transactional;
+// import javax.transaction.Transactional;
 import java.util.List;
 
 public interface PasienRestService {
+    PasienModel registerPasien(PasienModel pasien);
     List<PasienModel> retrieveListPasien();
     PasienModel updateSaldo(String id, int nominal);
     PasienModel retrievePasien(String idPasien);
